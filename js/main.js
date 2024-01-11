daa6a666179072cbdac807411acc497889a72ff4
sergejcervakov <mtsvideo1@gmail.com>
2024-01-11 10:02:27 +0600

    Fankeragroup v1

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() >= 100){
+            $('header').addClass('header-bg')
+        }   else if($('header-bg').hasClass('header-bg')){
+            $('header').removeClass('header-bg')
+        }
+    });
+
+    footer_height();
+
+})
+
+
+
+function footer_height() {
+    $('footer').css('height', $('.relative-content').height());
+}
+
+
